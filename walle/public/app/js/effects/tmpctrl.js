6a4830cdf30bd760958ddfd32abd9ea62b8a84f7
weixy <weixiny2008@gmail.com>
2014-05-17 21:19:39 +1200

    v0.2
    Add Facebook API practices.

--- walle/public/app/js/effects/tmpctrl.js
+++ walle/public/app/js/effects/tmpctrl.js
@@ -2,12 +2,68 @@
  * Created by weixy on 9/05/14.
  */
 var module = angular.module('TmptApp', ['ui.bootstrap'])
-    .controller('TmpCtrl', ['$scope', 'Tmpt', function (scope, Tmpt){
+    .controller('TmpCtrl', ['$scope', '$http', 'Tmpt', 'getCityList', 'queryWeather', function (scope, http, Tmpt, getCityList, queryWeather){
+
+        /*http.get('app/json/effects/cities.json').success(function(data) {
+            scope.cities = data;
+        });*/
+        var sentInSequence = function sentInSequence (name, id) {
+            console.log(scope.callWeatherflag);
+            if (scope.callWeatherflag == true) {
+                setTimeout(function() {sentInSequence(name, id);}, 200);
+            } else {
+                var weatherPromise = queryWeather.getWeather(name, id);
+                scope.callWeatherflag = true;
+                weatherPromise.then(function (result) {
+                    scope.cities[result.index].weather = result.result.data;
+                    scope.callWeatherflag = false;
+                });
+            }
+        };
+
+        scope.callWeatherflag = false;
+        var cityPromise = getCityList.getCities();
+        cityPromise.then(function(res) {
+            scope.cities = res;
+
+            for (var i = 0; i < scope.cities.length; i++) {
+                //sentInSequence(scope.cities[i].name, i);
+            }
+        });
+
         scope.$on('temperature.update', function(event) {
             scope.temp = Tmpt;
             scope.$apply();
         });
         scope.temp = Tmpt;
+    }])
+    .factory('getCityList', ['$http', '$q', function(http, q) {
+        var getCities = function() {
+            var cityDeferred = q.defer();
+            http.get('app/json/effects/cities.json').success(function(data) {
+
+            }).success(function(result) {
+                cityDeferred.resolve(result);
+            });
+            return cityDeferred.promise;
+        };
+        return {getCities: getCities}
+    }])
+    .factory('queryWeather', ['$http', '$q', function(http, q) {
+        var getWeather = function(cityName, id) {
+            var call_url = 'http://api.worldweatheronline.com/free/v1/weather.ashx?format=json&num_of_days=5&key=hn99mrhs26tefdr372s6vtcu&callback=JSON_CALLBACK';
+            var url = call_url + "&q=" + cityName;
+            console.log(url);
+            var deferred = q.defer();
+            http({
+                method: 'JSONP',
+                url: url
+            }).success(function(result){
+                deferred.resolve({result: result, index: id});
+            });
+            return deferred.promise;
+        };
+        return {getWeather: getWeather};
     }])
     .factory('Tmpt', ['$rootScope', function($rootScope){
         var temp = {
@@ -26,7 +82,6 @@ var module = angular.module('TmptApp', ['ui.bootstrap'])
             calculateColor: function(temp) {
                 var bk = temp.initTemp;
                 var de = temp.temperature.degree - 20;
-                console.log(de);
                 if (de > 0) {
                     switch(de) {
                         case 1:
@@ -83,7 +138,6 @@ module.directive('increaseTemp', ['Tmpt', function(Tmpt){
         link:function(scope, element, attrs) {
             element.bind("click", function() {
                 Tmpt.increaseTemp();
-                console.log("XXXXXXXX");
             });
         }
     }
@@ -94,7 +148,6 @@ module.directive('decreaseTemp', ['Tmpt', function(Tmpt){
         link:function(scope, element, attrs) {
             element.bind("click", function() {
                 Tmpt.decreaseTemp();
-                console.log("YYYYYYYY");
             });
         }
     }
